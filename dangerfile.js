1613c1c583840d79cc8115dfa097ff958735ff27
Ivan HA <hangolam@gmail.com>
2018-07-21 16:22:29 +0800

    fix: typo in dangerfile

--- dangerfile.js
+++ dangerfile.js
@@ -5,5 +5,5 @@ warn("This is a warning")
 message("This is a normal message")
 markdown("*Markdown* is also **supported**")
 
-const { additions = 0, delections = 0 } = danger.github.pr
-message(`:tada: The PR added ${additions} and removed ${delections} lines.`)
+const { additions = 0, deletions = 0 } = danger.github.pr
+message(`:tada: The PR added ${additions} and removed ${deletions} lines.`)
